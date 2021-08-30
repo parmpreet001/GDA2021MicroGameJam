a70cd514e801c6c5770590b11af7b3198fcfed57
Ryoma Marta-Sugawara <ryomams@users.noreply.github.com>
2021-08-29 17:19:44 -0700

    Niko Ball Spawning

---
+++ Assets/Scripts/nikorb.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nikorb : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        float xCord = Random.Range(0.1f, 3.0f);
+
+        if (Random.Range(0,10) % 2 == 0) {
+            xCord = xCord * -1;
+        }
+
+        transform.position = new Vector3(xCord, 5, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
